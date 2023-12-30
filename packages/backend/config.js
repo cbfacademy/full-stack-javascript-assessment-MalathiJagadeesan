4bf43030ac4a73e1c9fbe717052ededb6b6c3543
MalathiJagadeesan <140818265+MalathiJagadeesan@users.noreply.github.com>
2023-12-30 13:46:07 +0000

    initial backend development

---
+++ packages/backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 9012;
+
+export const mongoDBURL = "mongodb+srv://malathi:sweety%40123@book-store-mern.m0jsvfe.mongodb.net/books-collection?retryWrites=true&w=majority"
--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,38 +1,69 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+import express, { request, response } from "express" ;
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookmodel.js";
 
-require("dotenv").config();
+
+
 const app = express();
 
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-const uri = process.env.MONGO_URI; // Add your connection string from Atlas to your .env file. See https://docs.atlas.mongodb.com/getting-started/
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+app.get('/', (request,response) => {
+    console.log(request);
+    return response.status(234).send('Hello')
 });
 
-client.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MongoDB", err);
-    return;
-  }
-  console.log("Connected to MongoDB");
-  client.close();
+//Route for save a new Book
+app.post('/books', async(request, response) => {
+    try {
+        if(
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.title,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello from the CBF Academy backend!");
+//Route for Get All Books from database
+app.get('/books', async (request,response) => {
+    try{
+
+        const books = await Book.find({});
+
+        return response.status(200).json(books);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
+
+
+
+mongoose 
+.connect(mongoDBURL,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('App is connected to database');
+    app.listen(PORT,() =>{
+  console.log(`App is listening to port: ${PORT}`);
+
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+})
+.catch((error) => {
+    console.log(error);
 });
